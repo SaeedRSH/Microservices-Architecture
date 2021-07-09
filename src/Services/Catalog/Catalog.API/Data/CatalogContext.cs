cd4cb5c3dc9754686d441c0f0ec5909b5e1a2069
Saeed <Saeed@acddevelopment.com>
2021-07-09 17:27:44 +0800

    Catalog.API With MongoDB

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            try
+            {
+                var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+                Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+                CatalogContextSeed.SeedData(Products);
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex);
+            }
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
